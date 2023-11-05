9a3185ccbaefebab7a838871e5dac5aee8b95403
eunso0713 <aliennumber1one@sookmyung.ac.kr>
2023-11-05 20:46:26 +0900

    02

--- main.c
+++ main.c
@@ -3,18 +3,27 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+#define SIZE 5 
+
 int main(void) 
 {
-	int i;
-	int grade[5];
-	grade[0]=10;
-	grade[1]=20;
-	grade[2]=30;
-	grade[3]=40;
-	grade[4]=50;
+
+	int i, average;
+	int sum;
+	int grade[SIZE];
+
 	
-	for(i=0; i<5; i++)
-		printf("%d", grade[i]);
+	sum=0;
+	for (i=0; i<SIZE; i++)
+	{
+		printf("학생 성적을 입력하세요. \n");
+		scanf("%d", &grade[i]);
+		sum += grade[i];
+	}
+	
+	average = sum / SIZE;
+	printf("성적 평균: %d\n", average);
+	
 	
 	return 0;
 }
